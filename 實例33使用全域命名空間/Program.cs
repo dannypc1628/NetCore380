d5cbbe7a46f110523b8d24ae048932f40b470301
dannyLiu <h152074716@gmail.com>
2020-09-18 17:16:31 +0800

    加入實例33使用全域命名空間

---
+++ 實例33使用全域命名空間/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 實例33使用全域命名空間
+{
+    class Program
+    {
+        public class System { }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            global::System.Version v = new global::System.Version();
+        }
+    }
+}
