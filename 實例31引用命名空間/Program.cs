24f6f1411c501e33ae3c6e6930fcdf2d8ad7a7f4
dannyLiu <h152074716@gmail.com>
2020-09-18 17:15:55 +0800

    加入實例31引用命名空間

---
+++ 實例31引用命名空間/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace 實例31引用命名空間
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //現在如非必要，千萬不要使用ArrayList，請用List<T>
+            ArrayList myList = new ArrayList();
+
+            myList.Add("Tom");
+            myList.Add("Jim");
+            myList.Add("Jack");
+
+
+            foreach (object o in myList)
+            {
+                Console.WriteLine(o.ToString());
+            }
+            //List<T>範例
+            List<string> list = new List<string>();
+            list.Add("");
+            list.Add("Tom");
+            list.Add("Jim");
+            list.Add("Jack");
+
+            foreach (object o in list)
+            {
+                Console.WriteLine(o.ToString());
+            }
+        }
+    }
+}
