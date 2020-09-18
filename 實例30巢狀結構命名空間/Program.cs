8253bf564359e38a791c5f986e94249d12ff41af
dannyLiu <h152074716@gmail.com>
2020-09-18 17:15:42 +0800

    加入實例30巢狀結構命名空間

---
+++ 實例30巢狀結構命名空間/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace 實例30巢狀結構命名空間
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            NTest.NSub1.WorkTask v1 = new NTest.NSub1.WorkTask();
+
+            NTest.NSub2.Tool t1 = new NTest.NSub2.Tool();
+        }
+    }
+}
+
+namespace NTest
+{
+    namespace NSub1
+    {
+        class WorkTask
+        {
+
+        }
+    }
+
+    namespace NSub2
+    {
+        struct Tool
+        {
+
+        }
+    }
+}
+namespace NTest.NSub1
+{ 
+}
