6347fecbab010593885d0d08702cb3b3cad1157c
dannyLiu <h152074716@gmail.com>
2020-09-18 17:16:54 +0800

    加入實例34為引用的命名空間設定別名

---
+++ 實例34為引用的命名空間設定別名/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ext = Orgamization.Componetnt.Extensions;
+using mps = Orgamization.Componetnt.MainParts;
+
+namespace 實例34為引用的命名空間設定別名
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ext.BackgroundWork bw1 = new ext.BackgroundWork();
+            mps.BackgroundWork bw2 = new mps.BackgroundWork();
+
+        }
+    }
+}
+
+namespace Orgamization.Componetnt.Extensions
+{
+    public class BackgroundWork { }
+}
+
+namespace Orgamization.Componetnt.MainParts
+{
+    public class BackgroundWork { }
+
+}
