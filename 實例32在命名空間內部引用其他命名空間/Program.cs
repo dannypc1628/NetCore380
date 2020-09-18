7b51476997077e3d413343532624c4a84d832a6f
dannyLiu <h152074716@gmail.com>
2020-09-18 17:16:08 +0800

    加入實例32在命名空間內部引用其他命名空間

---
+++ 實例32在命名空間內部引用其他命名空間/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 實例32在命名空間內部引用其他命名空間
+{
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> list1 = new List<int> { 100, 200, 300 };
+
+            System.Collections.Generic.List<int> list2 = new System.Collections.Generic.List<int> { 100, 200, 300 };
+        }
+    }
+}
