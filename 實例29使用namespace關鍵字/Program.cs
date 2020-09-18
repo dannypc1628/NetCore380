bed8e1a7a1ea06e9da033597501db9199e9fd53e
dannyLiu <h152074716@gmail.com>
2020-09-18 17:15:27 +0800

    加入實例29使用namespace關鍵字

---
+++ 實例29使用namespace關鍵字/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 實例29使用namespace關鍵字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test.Car c = new Test.Car();
+            c.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace Test
+{
+    public class Car
+    {
+        public void Run()
+        {
+            Console.WriteLine("開車拉");
+        }
+    }
+}
