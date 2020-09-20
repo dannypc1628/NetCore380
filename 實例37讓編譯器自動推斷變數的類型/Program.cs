1057f6e3955622064ba93b17f19236a2b3f7ae5a
dannyLiu <h152074716@gmail.com>
2020-09-20 15:15:23 +0800

    實例37讓編譯器自動推斷變數的類型

---
+++ 實例37讓編譯器自動推斷變數的類型/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 實例37讓編譯器自動推斷變數的類型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = 123;
+            var d = 3.141d;
+            var f = 3.141f;
+            var m = 3.141m;
+            
+            d = (double)a;
+
+            Console.WriteLine($"變數a的類型為：{a.GetType().FullName}");
+        }
+    }
+}
