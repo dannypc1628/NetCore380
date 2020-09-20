1dbd0092aee7bd8038e5b6d6477bfab930aba6e8
dannyLiu <h152074716@gmail.com>
2020-09-20 15:16:07 +0800

    實例41為變數分配預設值

---
+++ 實例41為變數分配預設值/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 實例41為變數分配預設值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int v = default;
+            string s = default;
+            string nu = null;
+            Console.WriteLine($"int 類型的預設值:{v}");
+            Console.WriteLine(nu);
+            Console.WriteLine($"string 類型的預設值:{s??"這是null"}");
+
+        }
+    }
+}
