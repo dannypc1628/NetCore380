18f495e8cca2d4a254c26c9f2fda99f7e346bfa6
dannyLiu <h152074716@gmail.com>
2020-09-20 15:16:48 +0800

    實例42取得命令列參數

---
+++ 實例42取得命令列參數/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace 實例42取得命令列參數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            StringBuilder sbd = new StringBuilder();
+            sbd.AppendLine("接收到的命令列參數");
+
+            foreach(string a in args)
+            {
+                sbd.AppendFormat("這裡{0}<--,{1}", a,"333");
+            }
+            Console.WriteLine(sbd);
+
+            Console.ReadLine();
+        }
+    }
+}
