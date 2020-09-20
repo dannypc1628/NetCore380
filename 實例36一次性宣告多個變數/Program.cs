a2633ca28f850ca7ed3194753d2e1b7832a9d274
dannyLiu <h152074716@gmail.com>
2020-09-20 15:15:12 +0800

    實例36一次性宣告多個變數

---
+++ 實例36一次性宣告多個變數/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 實例36一次性宣告多個變數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 0;
+            int y;
+            int z;
+
+            int a, b, c;
+            int aa = 1, bb = 2, cc = 3;
+
+            string i ="apple", j="", k=null;
+
+
+            Console.WriteLine(x);
+        }
+    }
+}
