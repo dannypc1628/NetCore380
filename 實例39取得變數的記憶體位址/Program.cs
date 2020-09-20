6944269fe2ef553cb48b51f9957290c0538b5335
dannyLiu <h152074716@gmail.com>
2020-09-20 15:15:45 +0800

    實例39取得變數的記憶體位址

---
+++ 實例39取得變數的記憶體位址/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 實例39取得變數的記憶體位址
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte b = 255;
+
+            unsafe
+            {
+                byte* pb = &b;
+            }
+
+            int val = 200;
+            unsafe
+            {
+                int* p = &val;
+
+                IntPtr ptr = (IntPtr)p;
+                Console.WriteLine($"變數的地址：{ptr.ToString("x")}");
+                Console.WriteLine($"變數的地址：{ptr.ToString()}");
+
+            }
+        }
+    }
+}
