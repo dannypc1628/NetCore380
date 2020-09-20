8792d8aa142643670e6de925e37bbd4217b6912a
dannyLiu <h152074716@gmail.com>
2020-09-20 15:15:33 +0800

    實例38使用常數

---
+++ 實例38使用常數/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 實例38使用常數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const int x = 100;
+            Console.WriteLine(x);
+
+            
+            Console.WriteLine(x);
+        }
+    }
+}
