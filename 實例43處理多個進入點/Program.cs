0e1769e5c4b8a8756d1ef23f42409e2edf96938a
dannyLiu <h152074716@gmail.com>
2020-09-20 15:16:58 +0800

    實例43處理多個進入點

---
+++ 實例43處理多個進入點/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 實例43處理多個進入點
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Test
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("第二個進入點");
+            Console.ReadLine();
+        }
+    }
+}
