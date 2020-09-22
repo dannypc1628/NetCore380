499f243e051491a3cc7c53c3f06544b036978dfa
dannyLiu <h152074716@gmail.com>
2020-09-22 21:30:23 +0800

    實例23單行註解

---
+++ 實例23單行註釋/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 實例23單行註釋
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // 這是一個顯示Hello World的功能
+            // 第一行註釋
+            // 序列化一個物件
+            // 可以選用XML或JSON格式
+        }
+    }
+}
