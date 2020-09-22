6c896d67f4029a6f73ecb916540a2c70213a2a8f
dannyLiu <h152074716@gmail.com>
2020-09-22 21:30:32 +0800

    實例24多行註釋

---
+++ 實例24多行註釋/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 實例24多行註釋
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+            作者：Mr Lu
+            修訂日期：3月5日
+            修訂版本編號：v1.1
+            開發組：A組
+            */
+
+
+        }
+    }
+}
