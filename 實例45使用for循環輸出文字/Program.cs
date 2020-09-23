9a5b4d90b513de77bd1ea3caec947ee74ecbc3dc
dannyLiu <h152074716@gmail.com>
2020-09-23 18:12:34 +0800

    實例45使用for循環輸出文字

---
+++ 實例45使用for循環輸出文字/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 實例45使用for循環輸出文字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int n = 1; n<=5; n++)
+            {
+                Console.WriteLine($"這是第{n}行的文字");
+            }
+        }
+    }
+}
