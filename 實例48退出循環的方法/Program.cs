9819aee6c982790f8c7f8757903087c1f7d82b9c
dannyLiu <h152074716@gmail.com>
2020-09-24 12:36:27 +0800

    實例48退出循環的方法

---
+++ 實例48退出循環的方法/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 實例48退出循環的方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                Console.WriteLine("請按E來退出迴圈");
+
+                // false：顯示使用者按的鍵
+                // true ：不顯示使用者按的鍵
+                if (Console.ReadKey(false).Key == ConsoleKey.E)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
