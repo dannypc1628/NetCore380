5e13c7004452c013493020971369cb47daa35935
dannyLiu <h152074716@gmail.com>
2020-09-24 12:36:36 +0800

    實例49輸出20 以內能被3 整除的正整數

---
+++ 實例49輸出20 以內能被3 整除的正整數/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 實例49輸出20_以內能被3_整除的正整數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 1; i <=20; i++)
+            {
+                if( (i%3) != 0)
+                    continue;
+
+                Console.Write(" {0}",i);
+                Console.Write($" {i}");
+
+            }
+        }
+    }
+}
