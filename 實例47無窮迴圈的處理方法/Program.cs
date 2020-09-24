4e5bd37b33493a52c1ba366a27ea0ec9a87c338c
dannyLiu <h152074716@gmail.com>
2020-09-24 12:36:18 +0800

    實例47無窮迴圈的處理方法

---
+++ 實例47無窮迴圈的處理方法/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 實例47無窮迴圈的處理方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while 無窮迴圈
+            int i = 0;
+            while (9 == 9)
+            {
+                Console.WriteLine($"{DateTime.Now:T} 正在執行循環~~~{i}");
+                if (i > 100)
+                {
+                    break;
+                }
+
+                i++;
+            }
+            //for 無窮迴圈
+            //for (; true;)
+            //{
+            //    Console.WriteLine(123);
+            //}
+        }
+    }
+}
