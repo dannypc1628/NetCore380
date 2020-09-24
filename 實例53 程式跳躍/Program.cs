47a60c2fad2a31ffe28ba0183516e912d7cd5f81
dannyLiu <h152074716@gmail.com>
2020-09-25 02:21:05 +0800

    實例53 程式跳躍

---
+++ 實例53 程式跳躍/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace 實例53_程式跳躍
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var keyInfo = Console.ReadKey(false);
+
+            if (keyInfo.Key == ConsoleKey.LeftArrow)
+                goto left;
+            if (keyInfo.Key == ConsoleKey.RightArrow)
+                goto right;
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+                goto up;
+            if (keyInfo.Key == ConsoleKey.DownArrow)
+                goto down;
+            else
+                goto other;
+
+            left:
+            Console.WriteLine("你按下了左方向鍵");
+            Console.Read();
+            return;
+        right:
+            Console.WriteLine("你按下了右方向鍵");
+            Console.Read();
+            return;
+        up:
+            Console.WriteLine("你按下了上方向鍵");
+            Console.Read();
+            return;
+        down:
+            Console.WriteLine("你按下了下方向鍵");
+            Console.Read();
+            return;
+        other:
+            Console.WriteLine("你按下了其他方向鍵");
+            Console.Read();
+            return;
+
+        }
+    }
+}
