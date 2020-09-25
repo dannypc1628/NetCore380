b25f0a676a753f701fa2a52d571e16fe1aba3dfc
dannyLiu <h152074716@gmail.com>
2020-09-26 03:20:38 +0800

    實例54 宣告公共類別、並嘗試再外部組件呼叫類別

---
+++ 實例54 宣告公共類別/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 實例54_宣告公共類別
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Ant v1 = default(Ant);
+
+            Drangonfly v2 = default;
+
+            Spider v3 = default;
+
+        }
+    }
+
+    public class Ant { }
+
+    public class Drangonfly { }
+
+    public class Spider { }
+}
---
+++ 實例54 宣告公共類別測試internal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using 實例54_宣告公共類別;
+
+namespace 實例54_宣告公共類別測試internal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Ant a = new Ant();
+            實例54_宣告公共類別.Drangonfly d = new Drangonfly();
+            //實例54_宣告公共類別.Program p = new 實例54_宣告公共類別.Program(); //無法存取
+        }
+    }
+}
