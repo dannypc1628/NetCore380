1d88c112ad84efbbbbce827ecff35a872aa08092
dannyLiu <h152074716@gmail.com>
2020-09-28 20:41:06 +0800

    實例62 輸出參數

---
+++ 實例62 輸出參數/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 實例62_輸出參數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //double r;
+            Work(2.001d, 0.855d, out double r);
+
+            Console.WriteLine($"計算結果：{r}");
+        }
+
+        static void Work(double x ,double y, out double result)
+        {
+            result = x + y;
+        }
+    }
+}
