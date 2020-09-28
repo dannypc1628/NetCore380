774f5a73ee2ce9c43ba1c2ca282019074164e555
dannyLiu <h152074716@gmail.com>
2020-09-28 20:41:13 +0800

    實例63 可變個數的方法參數

---
+++ 實例63 可變個數的方法參數/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 實例63_可變個數的方法參數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = new[] { 1, 2, 3, 4 };
+            Sample(arr);
+
+            Console.WriteLine("\n\n使用可變個數的方法參數");
+            Sample(7, 8, 9, 10);
+        }
+        static void Sample(params int[] numbers)
+        {
+            Console.WriteLine("\n參數列表");
+            foreach (int i in numbers)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
