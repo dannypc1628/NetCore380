5912a37d713ff2b51ddb9cdfc19731c6da116d94
dannyLiu <h152074716@gmail.com>
2020-09-29 14:07:03 +0800

    實例66 可選參數

---
+++ 實例66 可選參數/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 實例66_可選參數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Something(20, 100, true);
+
+            Something(31, 1);
+
+            //Something(900, ,true);這是錯誤的寫法
+            Something(p1: 65, p3: true);
+
+        }
+
+        static void Something(int p1, byte p2 =255 ,bool p3 = false)
+        {
+            string msg = "參數列表\n" + $"{nameof(p1)} = {p1} \n{nameof(p2)} = {p2}"
+                        + $"\n{nameof(p3)} = {p3} ";
+            Console.WriteLine(msg);
+        }
+    }
+}
