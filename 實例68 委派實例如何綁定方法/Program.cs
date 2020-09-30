49b765fefdca260c920f10f08281b405b5f55573
dannyLiu <h152074716@gmail.com>
2020-09-30 23:51:10 +0800

    實例68 委派實例如何綁定方法

---
+++ 實例68 委派實例如何綁定方法/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 實例68_委派實例如何綁定方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DoSomething dele = RueHere;// 等同於 DoSomething dele = new DoSomething(RueHere);
+
+            double result = dele(16, 27.67d); //新版的寫法
+            double result2 = dele.Invoke(16, 27.67d); // C#1.0的寫法
+        }
+
+        delegate double DoSomething(int x,double y);
+
+        static double RueHere(int a , double b)
+        {
+            return a + b;
+        }
+    }
+}
