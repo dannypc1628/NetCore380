51760cba90e4d0148353515cbfdd6960a6f13a3f
dannyLiu <h152074716@gmail.com>
2020-09-30 23:51:25 +0800

    實例70 匿名方法

---
+++ 實例70 匿名方法/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace 實例70_匿名方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test td = null;
+            td += delegate (double x, double y)
+            {
+                Console.WriteLine($"{x} + {y} = {x + y}");
+            };
+
+            td += delegate (double x, double y)
+            {
+                Console.WriteLine($"{x} - {y} = {x - y}");
+            };
+
+            td += delegate (double x, double y)
+            {
+                Console.WriteLine($"{x} X {y} = {x * y}");
+            };
+            
+            td(0.3d, 0.2d);
+        }
+
+        delegate void Test(double f, double g);
+
+    }
+}
