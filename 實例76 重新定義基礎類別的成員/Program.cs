c5c11c53d8f69478b8effa30b9de201f7ea72dd2
dannyLiu <h152074716@gmail.com>
2020-10-03 01:03:46 +0800

    實例76 重新定義基礎類別的成員

---
+++ 實例76 重新定義基礎類別的成員/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 實例76_重新定義基礎類別的成員
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyTest t = new MyTest();
+            t.Outupt();
+        }
+    }
+
+    class MyBase
+    {
+        public virtual void Outupt()
+        {
+            Console.WriteLine("基礎類別的方法被呼叫");
+        }
+    }
+
+    class MyTest : MyBase
+    {
+        public override void Outupt()
+        {
+            Console.WriteLine("衍生類別的方法被呼叫");
+            base.Outupt();
+        }
+    }
+}
