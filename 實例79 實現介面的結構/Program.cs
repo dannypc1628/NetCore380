99989dd11db0751848bdf91f24cc2bd0d6e302c6
dannyLiu <h152074716@gmail.com>
2020-10-03 22:29:18 +0800

    實例79 實現介面的結構

---
+++ 實例79 實現介面的結構/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace 實例79_實現介面的結構
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ITest v = new TestCal();
+            
+            int result = v.Add(10, 5);
+            Console.WriteLine("計算結果：{0}",result);
+        }
+    }
+
+    interface ITest
+    {
+        int Add(int a, int b);
+    }
+
+    struct TestCal : ITest
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
