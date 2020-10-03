bbe2d0b0605706b535669b81b14d4f2047305c2f
dannyLiu <h152074716@gmail.com>
2020-10-03 22:29:24 +0800

    實例80 隱藏建置函數

---
+++ 實例80 隱藏建置函數/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace 實例80_隱藏建置函數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Camara c = Camara.CurrentInstance;//因為建構函式private所以無法從外部用new Camara()取得實例;
+            Console.WriteLine($"裝置標示：{c.DeviceID}");
+        }
+    }
+
+    class Camara
+    {
+        private Guid _deviceID;
+
+        private static Camara _currentInstance = new Camara();
+
+        public static Camara CurrentInstance => _currentInstance;
+
+        private Camara()
+        {
+            _deviceID = Guid.NewGuid();
+        }
+
+        public Guid DeviceID => _deviceID;
+    }
+}
