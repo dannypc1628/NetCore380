4316034c903289a75f0ef4c7c0554d1eba689e9c
dannyLiu <h152074716@gmail.com>
2020-10-03 22:28:29 +0800

    實例78 實現多個介面

---
+++ 實例78 實現多個介面/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace 實例78_實現多個介面
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            OneWork ow = new OneWork();
+            ow.StartWork();
+            ow.EndWork();
+
+        }
+    }
+
+    interface IRunner1
+    {
+        void StartWork();
+    }
+
+    interface IRunner2
+    {
+        void EndWork();
+    }
+    
+    class OneWork : IRunner1, IRunner2
+    {
+        public void StartWork()
+        {
+            Console.WriteLine("開始操作");
+        }
+
+        public void EndWork()
+        {
+            Console.WriteLine("結束操作");
+
+        }
+    }
+}
