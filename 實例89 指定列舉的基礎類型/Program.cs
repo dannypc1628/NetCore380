fea689bdc6bcdf5204fe1062bc4239d3a3c954b3
dannyLiu <h152074716@gmail.com>
2020-10-06 15:30:34 +0800

    實例89 指定列舉的基礎類型

---
+++ 實例89 指定列舉的基礎類型/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 實例89_指定列舉的基礎類型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public enum ReadMode : byte
+    {
+        NewFile = 1,
+        OpenCurrent = 2,
+        Saved = 3
+    }
+
+    public enum PictureQt : uint
+    {
+        HQ = 4,
+        LQ = 12,
+        MQ = 7
+    }
+    
+}
