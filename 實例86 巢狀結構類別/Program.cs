5f90fa0bdfc1a1a32c7adf61b51d21b14526a45f
dannyLiu <h152074716@gmail.com>
2020-10-06 15:30:01 +0800

    實例86 巢狀結構類別

---
+++ 實例86 巢狀結構類別/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace 實例86_巢狀結構類別
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TheOut obj1 = new TheOut();
+            obj1.CallNest();
+            TheOut.TheNest obj2 = new TheOut.TheNest();
+            obj1.NestObj = obj2;
+
+            obj1.CallNest();
+        }
+    }
+
+    public class TheOut
+    {
+        public TheNest NestObj { get; set; }
+
+        public void CallNest()
+        {
+            if (NestObj != null)
+            {
+                NestObj.CallMe();
+            }
+            else
+            {
+                Console.WriteLine("沒有NestObj實體");
+            }
+        }
+        public class TheNest
+        {
+            public void CallMe() => Console.WriteLine("正在存取巢狀結構的類別實例");
+        }
+    }
+}
