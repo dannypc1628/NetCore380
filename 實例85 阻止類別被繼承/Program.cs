09486ec867cd73e00cd797752b1b0c27c41778d9
dannyLiu <h152074716@gmail.com>
2020-10-06 15:29:54 +0800

    實例85 阻止類別被繼承

---
+++ 實例85 阻止類別被繼承/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 實例85_阻止類別被繼承
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    sealed class Pear 
+    {
+
+    }
+
+    class WildPear : Pear //密封類別無法被繼承
+    {
+
+    }
+}
