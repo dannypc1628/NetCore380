963462dd34a2050431ba435bc8215aa3d5c9021d
dannyLiu <h152074716@gmail.com>
2020-10-06 15:30:08 +0800

    實例87 匿名類型

---
+++ 實例87 匿名類型/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 實例87_匿名類型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var d = new
+            {
+                Color = "白色",
+                Size = 43.6f,
+                Number = 7988
+            };
+            //d.Color = "sdfsf"; 無法修改唯獨屬性
+
+            string str = $"顏色：{d.Color}\n 尺碼：{d.Size} \n 編號：{d.Number}";
+
+            Console.WriteLine("商品資訊{0}", str);
+        }
+    }
+}
