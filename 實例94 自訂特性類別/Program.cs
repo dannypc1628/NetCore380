34b09d3b0dd2bb623ef89b18efee218536c850a9
dannyLiu <h152074716@gmail.com>
2020-10-08 22:37:02 +0800

    實例94 自訂特性類別

---
+++ 實例94 自訂特性類別/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 實例94_自訂特性類別
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
