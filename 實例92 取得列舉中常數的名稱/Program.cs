58541d29c98504c4a515163920a9f44e6aab66d8
dannyLiu <h152074716@gmail.com>
2020-10-08 22:36:43 +0800

    實例92 取得列舉中常數的名稱

---
+++ 實例92 取得列舉中常數的名稱/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 實例92_取得列舉中常數的名稱
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Enum.GetName(typeof(DayOfWeek),1));
+            Console.WriteLine(Enum.GetName(typeof(DayOfWeek), 5));
+
+            string[] days = Enum.GetNames(typeof(DayOfWeek));
+
+            foreach(string d in days)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+
+}
