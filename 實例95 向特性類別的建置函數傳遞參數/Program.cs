b75db6484271d6eea19e37362d72bed363ccd4e5
dannyLiu <h152074716@gmail.com>
2020-10-08 22:37:11 +0800

    實例95 向特性類別的建置函數傳遞參數

---
+++ 實例95 向特性類別的建置函數傳遞參數/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 實例95_向特性類別的建置函數傳遞參數
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
