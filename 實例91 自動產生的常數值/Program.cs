bd237e95c71dc5f62e8dace0f34002540e6833fd
dannyLiu <h152074716@gmail.com>
2020-10-08 22:36:34 +0800

    實例91 自動產生的常數值

---
+++ 實例91 自動產生的常數值/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace 實例91_自動產生的常數值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"{Type.Run} => {(int)Type.Run}");
+            Console.WriteLine($"{Type.Stop} => {(int)Type.Stop}");
+            Console.WriteLine($"{Type.Close} => {(int)Type.Close}");
+
+            Example y = Example.ItemA;
+            Console.WriteLine($"{y} = {(int)y}");
+            y = Example.ItemB;
+            Console.WriteLine($"{y} = {(int)y}");
+            y = Example.ItemD;
+            Console.WriteLine($"{y} = {(int)y}");
+            Console.WriteLine($"{Example.ItemE} = {(int)Example.ItemE}");
+        }
+    }
+
+    enum Example
+    {
+        ItemA = 3,
+        ItemB,
+        ItemC = 10,
+        ItemD,
+        ItemE,
+    }
+
+    enum Type
+    {
+        Run,
+        Stop = 3,
+        Close
+    }
+}
