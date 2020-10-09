83d9c18b66dc255ed34a69127d53802393bf4697
dannyLiu <h152074716@gmail.com>
2020-10-09 17:14:24 +0800

    實例96 在同一物件上應用多個特性實例

---
+++ 實例96 在同一物件上應用多個特性實例/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 實例96_在同一物件上應用多個特性實例
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Docs a = new Docs();
+        }
+    }
+    //[Obsolete]
+    [Obsolete]
+    class Docs
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class,AllowMultiple = true)]
+    public class CustomAttribute : Attribute
+    {
+        public string Ver { get; set; }
+    }
+
+    [Custom(Ver = "1.0.0.2")]
+    [Custom(Ver="1.0.0.1"), Custom(Ver = "1.0.0.3")]
+    public class AppData
+    {
+
+    }
+}
