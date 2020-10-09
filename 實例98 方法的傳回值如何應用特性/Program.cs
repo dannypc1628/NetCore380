152bc0253c40727723bc8a813f7ff4a234229616
dannyLiu <h152074716@gmail.com>
2020-10-09 17:15:03 +0800

    實例98 方法的傳回值如何應用特性

---
+++ 實例98 方法的傳回值如何應用特性/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 實例98_方法的傳回值如何應用特性
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        [return:CheckSum]
+        static string SaySomeing()
+        {
+            return "Hello";
+        }
+    }
+
+    //[My]
+    public class Checker
+    {
+        //[My]
+        public int SomeValue { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.ReturnValue)]
+    public class CheckSumAttribute : Attribute
+    {
+
+    }
+}
