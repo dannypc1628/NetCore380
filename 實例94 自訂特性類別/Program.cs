972a4efdf1619ce253c205a288e1d385f062cb43
dannyLiu <h152074716@gmail.com>
2020-10-09 17:13:49 +0800

    修正實例94 自訂特性類別commit錯誤

--- 實例94 自訂特性類別/Program.cs
+++ 實例94 自訂特性類別/Program.cs
@@ -7,6 +7,30 @@ namespace 實例94_自訂特性類別
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            OrderData o = new OrderData();
+            o.OrdID = 123;
+        }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Property | AttributeTargets.Method)]
+    public class MyDemoAttribute : Attribute
+    {
+        public string Description { get; set; }
+    }
+
+    public class OrderData
+    {
+        [MyDemo(Description = "訂單ID")]
+        public int OrdID { get; set; }
+
+        [MyDemo(Description = "增加時間")]
+        public DateTime AddTime { get; set; }
+
+
+        [MyDemo(Description = "計算折扣價")]
+        public double Compute(double q)
+        {
+            return q * 0.98d;
         }
     }
 }
