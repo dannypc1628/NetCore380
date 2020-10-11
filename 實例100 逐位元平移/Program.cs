3a9c5ce2601017db4dce1b1bba9cc1dc93c0262e
dannyLiu <h152074716@gmail.com>
2020-10-11 18:08:27 +0800

    實例100 逐位元平移

---
+++ 實例100 逐位元平移/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 實例100_逐位元平移
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 305;
+            int y = 1060;
+            Console.WriteLine($"{Convert.ToString(x,8)} 向左移動3位數，" +
+                $"獲得{Convert.ToString( x << 3, 2)}");
+            Console.WriteLine($"{Convert.ToString(y, 16)} 向右移動4位數，" +
+                $"獲得{Convert.ToString(y >> 4, 2)}");
+        }
+    }
+}
