47a625bbab12dfc47564992beaf76f3c50084e87
dannyLiu <h152074716@gmail.com>
2020-10-11 15:32:03 +0800

    實例99 計算一個整數的階乘

---
+++ 實例99 計算一個整數的階乘/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace 實例99_計算一個整數的階乘
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int n = 4;
+            long r = Compute(n);
+
+            Console.WriteLine($"{n} 的階乘為{r}");
+        }
+
+        static long Compute(int num)
+        {
+            if (num == 0)
+            {
+                return -1L;
+            }
+            long result = 1L;
+            int temp = 1;
+            //  5! = 1 x 2 x 3 x 4 x5
+            while (temp <= num)
+            {
+                result = result * temp;
+                temp++;
+            }
+            return result;
+        }
+    }
+}
