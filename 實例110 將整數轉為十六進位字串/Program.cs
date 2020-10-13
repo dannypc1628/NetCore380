ebfba41f2dd3ddefc9a2fc7834be7affbe16c300
dannyLiu <h152074716@gmail.com>
2020-10-13 16:00:23 +0800

    實例110 將整數轉為十六進位字串

---
+++ 實例110 將整數轉為十六進位字串/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 實例110_將整數轉為十六進位字串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 91545588;
+            Console.WriteLine("{0}-> Ox{1}",x,x.ToString("x2"));
+            Console.WriteLine("{0}-> Ox{1}", x, x.ToString("u"));
+
+        }
+    }
+}
