47785489d31d699c96c533cf31b439f49fd0c812
dannyLiu <h152074716@gmail.com>
2020-10-13 12:49:43 +0800

    實例106 將int 數值自動轉型為double 數值

---
+++ 實例106 將int 數值自動轉型為double 數值/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 實例106_將int_數值自動轉型為double_數值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 28, y = 66, z = 312;
+            int i = 12;
+
+            double e = x, f = y, g = z;
+            double j = (double)i;
+
+            Console.WriteLine($"{x,-15} {e,-15}");
+            Console.WriteLine($"{y,-15} {f,-15:0.000}");
+            Console.WriteLine($"{z,-15} {g,-15:0.000}");
+
+
+        }
+    }
+}
