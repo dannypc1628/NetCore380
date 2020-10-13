0175e279baa3ab2679ebfd93c45c850c1bb384c4
dannyLiu <h152074716@gmail.com>
2020-10-13 12:49:58 +0800

    實例108 將位元組陣列轉為字串

---
+++ 實例108 將位元組陣列轉為字串/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 實例108_將位元組陣列轉為字串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte[] buffer = { 3, 12, 5, 92, 7, 61, 18, 53, 135 };
+
+            string str = BitConverter.ToString(buffer);
+
+            Console.WriteLine(str);
+        }
+    }
+}
