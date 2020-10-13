0edf7deb79dd0482036b988728188c0668a1c2f5
dannyLiu <h152074716@gmail.com>
2020-10-13 12:49:29 +0800

    實例105 強制轉換

---
+++ 實例105 強制轉換/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 實例105_強制轉換
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double dd = 1.23;
+            int k = (int)dd;
+
+            double a = 5.10985d;
+            int b = (int)a;
+            Console.WriteLine($"{a,-15}{b,-15}");
+
+            double c = 12.8155d;
+            int d = (int)c;
+            Console.WriteLine($"{c,-15}{d,-15}");
+        }
+    }
+}
